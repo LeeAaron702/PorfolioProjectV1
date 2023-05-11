97c444103267f203e25871aa45c5c6640d2ac200
Lee Seaver <lee.seaver@gmail.com>
2023-05-11 03:49:29 -0700

    personalized 7

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-sky-950 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
