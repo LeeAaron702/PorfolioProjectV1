eb5b0b4d15b25b76e585cb5335f4fb52fad1403f
Lee Seaver <lee.seaver@gmail.com>
2023-05-11 03:43:27 -0700

    personalized 6

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -75,9 +75,9 @@ const AboutSection = () => {
             <Image
               src={"/about-image.png"}
               alt=""
-              width={325}
-              height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+              width={600}
+              height={600}
+              className="hidden md:block md:relative md:bottom-4 md:left-2 md:z-0"
             />
           </div>
         </div>
