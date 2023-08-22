cd678ca3988a587b3463692ece826e9411ab29a3
Lee Seaver <lee.seaver@gmail.com>
2023-08-21 22:08:17 -0700

    styling

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -11,12 +11,12 @@ const HeroSection = () => {
 
   return (
     <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-20 md:flex-row md:space-x-4 md:text-left">
       <div className="md:mt-2 md:w-1/2">
             <Image  src={"/Headshot.png"} alt="" width={300} height={300} className="rounded-full shadow-2xl"/>
         </div>
         <div className="md:mt-2 md:w-3/5">
-        <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Lee</h1>
+        <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Lee.</h1>
         <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl"> I like to build things for the web.</h1>
             <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
